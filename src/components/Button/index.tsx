521dd4de195f4d0516dacc6146e7a184839e173b
FelipeMMoraes <felipe.moraes8@hotmail.com>
2023-07-16 02:49:00 -0300

    refactor: refatorando funçao do component Button

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -2,19 +2,13 @@ import * as S from './styled'
 
 import type { ButtonProps } from './types'
 
-const Button: React.FC<ButtonProps> = ({
-  children,
-  isActive = false,
-  icon: Icon
-}) => {
-  return (
-    <S.NavbarLinkContainer>
-      <S.NavbarLink isActive={isActive}>
-        {Icon && <Icon size={24} />}
-        {children}
-      </S.NavbarLink>
-    </S.NavbarLinkContainer>
-  )
-}
+const Button = ({ children, isActive = false, icon: Icon }: ButtonProps) => (
+  <S.NavbarLinkContainer>
+    <S.NavbarLink isActive={isActive}>
+      {Icon && <Icon size={24} />}
+      {children}
+    </S.NavbarLink>
+  </S.NavbarLinkContainer>
+)
 
 export { Button }
